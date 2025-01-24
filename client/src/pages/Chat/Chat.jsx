d2b6cfda88f54a8d3a6d12b5280540c73cc01d27
Felice Micillo <felicemicillo13@gmail.com>
2025-01-24 18:32:42 +0100

    Update Chat.jsx

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -204,7 +204,7 @@ function Chat({socket}){
                     style={{ width: '100%', height: "400px", border: '1px solid black' }} />
                 </div>
             </div>
-            <button>CIAOO</button>
+      
             <TextChat
             messages={messages}
             setMessages={setMessages}  
