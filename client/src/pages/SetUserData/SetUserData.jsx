031777f3cafcbb222969a7c6ea27ac8fc29cfc37
Francesco Scognamiglio <francosco@hotmail.it>
2025-02-05 18:04:47 +0100

    Pagina di inserimento informazioni

--- client/src/pages/SetUserData/SetUserData.jsx
+++ client/src/pages/SetUserData/SetUserData.jsx
@@ -2,12 +2,14 @@ import { useNavigate } from "react-router-dom";
 import "./SetUserData.css";
 import { useEffect, useState } from "react";
 import UploadPhoto from "../../component/UploadPhoto/UploadPhoto";
-import { Divider, TextField, Typography } from '@mui/material';
+import { Button, Divider, TextField, Typography, useMediaQuery } from '@mui/material';
+import EmptyImage from "../../assets/images-empty.png";
 
 function SetUserData(){
 
     //Inizializzazione variabili
     const navigate = useNavigate();
+    const mobile = useMediaQuery("(max-width: 800px)")
 
     const [userData, setUserData] = useState({});
     const [nickname, setNickname] = useState("");
@@ -29,15 +31,22 @@ function SetUserData(){
 
     //Funzione: quando cambia l'imput del nickname imposta la variabile
     const onChangeNickname = (e) => {
-        let userDataTmp = userData;
-        userDataTmp = {...userData, nickname: e.target.value};
-        localStorage.setItem("userData", JSON.stringify(userDataTmp));
         setNickname(e.target.value);
     }
 
     //Funzione: apre il dialog per l'upload della foto
     const onClickUploadPhoto = (e) => {
         setOpenUploadPhoto(true);
+    }
+
+    const onClickRemoveImage = () => {
+        setImage(null);
+        if(nickname){
+            const userData = {nickname: nickname};
+            localStorage.setItem("userData", JSON.stringify(userData));
+        }else{
+            localStorage.setItem("userData", JSON.stringify({}));
+        }
     }
 
     //Funzione: imposta i dati immessi dall'utente e li inserisce nel localstorage
@@ -70,28 +79,40 @@ function SetUserData(){
     return(
         <div className="setuserdata-div">
             <Typography variant="h3" className="title">Informazioni utente</Typography>
-            <div className="description-div">
-                <Typography className="description" >
-                    Scegli un nickname e carica una foto che ti rappresenti.
-                    Queste informazioni saranno visibili all'utente con cui verrai abbinato,
-                    permettendogli di decidere se accettare o meno la tua richiesta di connessione.
-                </Typography>
-            </div>
-            <br />
 
             <div className="form-div">
 
                 <Typography className="form-title">Imposta i tuoi dati</Typography>
 
-                <Divider className="divider" textAlign="left">Inserisci il nickname</Divider>
+                <div className="description-div">
+                    <Typography className="description" >
+                        Scegli un nickname e carica una foto che ti rappresenti.
+                        Queste informazioni saranno visibili all'utente con cui verrai abbinato,
+                        permettendogli di decidere se accettare o meno la tua richiesta di connessione.
+                    </Typography>
+                </div>
+
+                <Divider className="divider" textAlign="left">
+                    <Typography>Inserisci il nickname</Typography>
+                </Divider>
 
                 <TextField onChange={onChangeNickname} value={nickname} size="small" className="textfield" label="Nickname" />
             
+                <Divider className="divider" textAlign="left">
+                    <Typography>Inserisci un'immagine</Typography>
+                </Divider>
+
+                <div className="image-buttons-div">
+                    <Button size={mobile ? "small":"medium"} variant="contained" onClick={onClickUploadPhoto} >Carica immagine</Button>
+                    {image && (
+                        <Button size={mobile ? "small":"medium"} color="error" variant="contained" onClick={onClickRemoveImage} >Elimina immagine</Button>
+                    )}
+                </div>
+
                 <div className="selected-image-div">
-                    <img src={image} />
+                    <img src={image ? image : EmptyImage} style={{width: (!image && !mobile ) ? "400px":"100%"}}/>
                 </div>
                 
-                <button onClick={onClickUploadPhoto}>Inserisci immagine</button>
                 {
                     openUploadPhoto && (
                         <UploadPhoto 
@@ -103,8 +124,10 @@ function SetUserData(){
                     )
                 }
 
-                <button onClick={onClickReset}>Reset</button>
-                <button onClick={onClickChat} disabled={!image || !nickname}>Prosegui</button> 
+                <div className="main-buttons-div">
+                    <Button size={mobile ? "small":"medium"} variant="contained" onClick={onClickChat} disabled={!image || !nickname} >Prosegui</Button>
+                    <Button size={mobile ? "small":"medium"} color="error" variant="contained" onClick={onClickReset} >Elimina dati</Button>
+                </div>
 
             </div>  
         </div>
